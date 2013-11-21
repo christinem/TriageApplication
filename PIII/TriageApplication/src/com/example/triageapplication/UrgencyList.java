9ad497e0701498c76502d5cd68cc80b8e70e76cf
c1sachsn <c1sachsn@37192f84-ffa8-4790-93cd-f6035bec326f>
2013-11-21 03:08:42 +0000

    added urgencylist class

---
+++ PIII/TriageApplication/src/com/example/triageapplication/UrgencyList.java
@@ -0,0 +1,143 @@
+package com.example.triageapplication;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.text.Collator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+import android.content.Context;
+
+public class UrgencyList {
+	private Queue urgencyList;
+
+	
+	public UrgencyList(){
+		this.urgencyList = new PriorityQueue(); 
+	}
+
+	//add patients by decreasing order
+	//
+	public void addPatient(Record patient){
+		if (patient){//if this paitent has not seen the doctor left
+			PriorityQueue tempListBefore = new PriorityQueue();
+			PriorityQueue tempListAfter = new PriorityQueue();
+			
+			while (this.urgencyList.peek()!= null){//run through all of the objects
+					if (this.urgencyList.peek().urgency == patient.urgency){
+						while (this.urgencyList.peek()!= null){
+							tempListAfter.add(this.urgencyList.poll());//grabs remaining objects
+						
+						}
+						
+						break;
+					} else{
+						tempListBefore.add(this.urgencyList.poll());//Removes from the original list places it into storage
+						
+					}
+			
+					this.urgencyList.clear();//empties to prevent duplicates
+					
+					this.urgencyList = tempListBefore;
+					this.urgencyList.add(patient);
+					
+					
+					//Queue don't allow popping from the back this reorders 
+					Patient [] tempListAfterArray = tempListAfter.toArray();
+					
+					for (int i = 0; tempListAfterArray.length()> i; i++){
+						this.urgencyList.add(tempListAfterArray[tempListAfterArray.length-i]);
+						
+					}
+			}}
+	}
+	
+	
+	// also remvoes
+	//probably gets called when seen by doctor 
+	
+	/** Grabs the lowest element
+	 * @return
+	 */
+	public Record nextPatient(){
+		if (this.urgencyList.size()>0){		
+			return this.urgencyList.poll();
+		}
+	}
+
+	//need to grab null pointer exception possibly
+	public Record grabPatient(String healthCardNum){
+		if (this.urgencyList.contains(healthCardNum)){//need to modify once erin is done
+			Record [] iteratorArray = this.urgencyList.toArray();
+			
+			for (int i=0; i< iteratorArray.length; i++){
+				//basically if the patient with this health card matches current objects
+				//return that object
+			}
+		}
+	}
+	
+	//kind of extra but it easy implement remvoes a given patietn
+	//probably gets called when seen by doctor 
+	public void removePatient(String healthCardNum){
+		if (this.urgencyList.contains(healthCardNum)){//need to modify once erin is done
+			this.urgencyList.remove(healthCardNum);
+		}		
+	}
+	
+	public void sort(){
+		Record [] sortingArray = this.urgencyList.toArray();
+		
+		sortHelper(sortingArray);
+		
+		
+		
+		//grab a pivot
+	}
+	
+	public void sortHelper(Record [] array){
+		Record smallest = array[0];
+		Record temp;
+		
+		for (int z=0; array.length>z; z++){
+			for (int y=0; array.length>
+			if array[i]<
+		}		
+	}
+		
+	public void populate(String filePath, Context context) throws 
+    FileNotFoundException {
+        
+        Scanner scanner = new Scanner(new FileInputStream(filePath));
+        String[] record;
+        
+        while (scanner.hasNextLine()) {
+        	record = scanner.nextLine().split(",");
+        	String[] name = {record[0], record[1]};
+        	String healthCardNumber = record[2];
+        	String[] dob = {record[3], record[4], record[5]};
+        	Double temperature = Double.parseDouble(record[6]);
+        	int bloodPressure = Integer.decode(record[7]);
+        	int heartRate = Integer.decode(record[8]);
+        	String symptoms = record[9];
+        	Boolean seenByDoctor = Boolean.getBoolean(record[10]);
+        	String arrivalTime  = record[11];
+        	
+        	if (seenByDoctor != true){
+        		Record r = new Record(name, healthCardNumber, dob);
+        		r.setBloodPressure(bloodPressure);
+        		r.setHeartRate(heartRate);
+        		r.setSeenByDoctor(seenByDoctor);
+        		r.setSymptoms(symptoms);
+        		r.setTemperature(temperature);
+        		r.setArrivalTime(arrivalTime);
+    		
+        		urgencyList.add(r);
+        	}
+        }
+        scanner.close();
+        
+        sort();
+    }
+}
