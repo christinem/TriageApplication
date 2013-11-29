d509dfe9ac815699ed41b0db617d4cbcfd7a45bd
c1sachsn <c1sachsn@37192f84-ffa8-4790-93cd-f6035bec326f>
2013-11-29 01:11:48 +0000

    Updated JavaDoc Pharmacist.java 8:11

--- src/com/example/triageapplication/Pharmacist.java
+++ src/com/example/triageapplication/Pharmacist.java
@@ -6,20 +6,31 @@ import android.content.Context;
 
 public class Pharmacist extends StaffMember {
 
+	/** Constructs a Pharmacist that is also a StaffMember with a username
+	 * @param username This Pharmacist's username.
+	 */
 	public Pharmacist(String username) {
 		super(username);
 	}
 	
+	/** Fills a prescription from a doctor for a patient.
+	 * @param context This activities' context.
+	 * @return
+	 */
 	public String fillPerscription(Context context){
 		String prescription = null;
+		
 		try {
 			prescription = super.getPrescription(context);
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 		return prescription;
 	}
 	
+	/* Return's this user's type as a String  */
 	@Override
 	public String toString() {
 		return "Pharmacist";
