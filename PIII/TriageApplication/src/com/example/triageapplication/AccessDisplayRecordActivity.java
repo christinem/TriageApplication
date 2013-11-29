792e659e19272bcf6059c3244f4a30195a4fcd00
c1sachsn <c1sachsn@37192f84-ffa8-4790-93cd-f6035bec326f>
2013-11-29 00:32:56 +0000

    Updated JavaDocs AccessDisplayRecordActivity.java 7:32

--- PIII/TriageApplication/src/com/example/triageapplication/AccessDisplayRecordActivity.java
+++ PIII/TriageApplication/src/com/example/triageapplication/AccessDisplayRecordActivity.java
@@ -8,8 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 
 /**
- * This activity also allows access to patient's record, but for displaying information, 
- * instead of updating.
+ * This activity also allows access to patient's record,
+ *  for viewing not modifying.
  */
 public class AccessDisplayRecordActivity extends Activity {
 
@@ -21,28 +21,36 @@ public class AccessDisplayRecordActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds items to 
+		// the action bar if it is present.
 		getMenuInflater().inflate(R.menu.access_display_record, menu);
 		return true;
 	}
 	
+	/** This generates this activities display. Allowing records to be viewed. 
+	 * @param view This activities screen.
+	 */
 	public void display(View view){
 		Intent intentStaff = getIntent();
-	    StaffMember staff = (StaffMember) intentStaff.getSerializableExtra("staff");
+	    StaffMember staff = (StaffMember) 
+	    		intentStaff.getSerializableExtra("staff");
 	    
-	    EditText healthCardNum = (EditText) findViewById(R.id.idnumber_display);
+	    EditText healthCardNum = 
+	    		(EditText) findViewById(R.id.idnumber_display);
+	    
 	    String healthNum = healthCardNum.getText().toString();
 	    
 	     try {
 			//If this record exists retrieve it.  
 			Record record = staff.getRecord(healthNum);
 			Intent intent = new Intent(this, DisplayInformationActivity.class);
-		    //intent.putExtra("healthcardnumber", healthNum);
+		    
+			//intent.putExtra("healthcardnumber", healthNum);
 		    intent.putExtra("record", record);
 			intent.putExtra("staff", staff);
 		    startActivity(intent);
-		//FIX THIS - Add something when record does not exist
-		} catch (NoRecordSpecifiedException e) {
+
+	     } catch (NoRecordSpecifiedException e) {
 			// Record doesn't exist try to retrieve one that does.
 			   Intent reenter = new Intent(this, 
 					   ForceAccessDisplayActivity.class);
