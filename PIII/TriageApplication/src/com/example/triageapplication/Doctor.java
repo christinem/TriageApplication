4199b9ff988aa2447e45cca55b959891b705a745
c1sachsn <c1sachsn@37192f84-ffa8-4790-93cd-f6035bec326f>
2013-11-29 00:44:52 +0000

    Updated JavaDocs Doctor.java 7:44

--- PIII/TriageApplication/src/com/example/triageapplication/Doctor.java
+++ PIII/TriageApplication/src/com/example/triageapplication/Doctor.java
@@ -2,13 +2,17 @@ package com.example.triageapplication;
 
 import java.io.Serializable;
 
+
+/** This is a doctor class and is one of the user types for this
+ *  application's login system */
 public class Doctor extends StaffMember implements Serializable{
 
-	/**
-	 * 
-	 */
+    /** A unique ID for serialization. */
 	private static final long serialVersionUID = -4659829461859306830L;
 
+	/** Constructs a doctor that is also a StaffMember and has a username. 
+	 * @param username This doctor's username.
+	 */
 	public Doctor(String username) {
 		super(username);
 	}
